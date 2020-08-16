cf8138acc6dc448cfa84699dddf94acd845a2bcc
tslwoainil <tslwoainio@gmail.com>
2020-08-16 13:15:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
-sed -i '$a src-git small https://github.com/kenzok8/small
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
+#sed -i '$a src-git small https://github.com/kenzok8/small
